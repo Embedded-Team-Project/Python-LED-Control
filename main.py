cb6157b18643d4370e385152872cf818cdfc2afa
BetterTomorrowThanToday <wsx0239@gmail.com>
2023-11-26 20:39:05 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def display_message():
+    message = request.data.decode('UTF-8')
+    print(message)
+    return 'Message received'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
