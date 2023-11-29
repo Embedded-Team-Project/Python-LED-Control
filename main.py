b214809a10047066c5ae50e4899ccf9a8b39d5e6
임동혁 <idh1007@naver.com>
2023-11-29 19:10:45 +0900

    .

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask, request
+import RPi.GPIO as GPIO
+import time
+
 
 app = Flask(__name__)
-
+ledPin = 6
 
 @app.route('/', methods=['GET', 'POST'])
 def display_message():
-    message = request.data.decode('UTF-8')
-    print(message)
-    return 'Message received'
+    return hello()
 
+def hello():
+    GPIO.setmode(GPIO.BCM)  # setmode를 BCM으로 설정
+    GPIO.setup(ledPin, GPIO.OUT)  # 21번핀을 설정
+
+    GPIO.output(ledPin, 1)  # 처음에는 일단 꺼둔 상태
+    time.sleep(5)
+    GPIO.output(ledPin, 0)  # 처음에는 일단 꺼둔 상태
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
